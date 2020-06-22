63fdc8b6e998fbf7f4cedcef99b4c50165dcb2d3
Dipu Chandra Dey <dipudey764@gmail.com>
2020-06-22 22:12:29 +0600

    change customer field name

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,7 +15,7 @@ class ReviewResource extends JsonResource
     public function toArray($request)
     {
         return [
-          'customer' => $this->coustomer,
+          'customer' => $this->customer,
           'review' => $this->review,
           'star' => $this->star,
         ];
