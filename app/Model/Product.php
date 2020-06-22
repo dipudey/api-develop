0b10bc21bfdc3cd722cb97c1c0824127c78613ec
Dipu Chandra Dey <dipudey764@gmail.com>
2020-06-22 21:19:41 +0600

    Product Updated

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+      'name','details','price','stock','discount'
+    ];
 
     public function review(){
       return $this->hasMany(Review::class);
